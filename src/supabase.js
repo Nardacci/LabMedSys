b5a31f9b4ae5d414789d90b419ec9bfb9a697ab2
André Nardacci <56738656+Nardacci@users.noreply.github.com>
2026-09-01 09:57:37 -0300

    feat(supabase): add client configuration

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://qasjgklmivxpisqfhngx.supabase.co',
+  'sb_publishable_o6kUCnYclOY9AIlE1qNZAA_EwsFNXCM'
+);
